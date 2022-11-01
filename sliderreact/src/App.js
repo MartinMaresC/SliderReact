4b12b0f9a55c6b05595c55af8214c85548014f4b
Martin <martinmarescr@gmail.com>
2022-10-31 19:41:27 -0600

    Removes unsued dependencies

--- sliderreact/src/App.js
+++ sliderreact/src/App.js
@@ -2,7 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  
+  const slides =n [
+    {url: 'http://localhost:3000/.jpg', title: },
+    {url: 'http://localhost:3000/.jpg', title: },
+    {url: 'http://localhost:3000/.jpg', title: },
+    {url: 'http://localhost:3000/.jpg', title: },
+    {url: 'http://localhost:3000/.jpg', title: },
+  ];
 }
 
 export default App;
